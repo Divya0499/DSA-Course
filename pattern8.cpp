d213b3befd1eff232bc37b65c6528b046c343fcf
Divya <87847248+Divya0499@users.noreply.github.com>
2022-01-10 22:46:48 +0530

    Add files via upload

---
+++ pattern8.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int n;
+cin>>n;
+
+for( int i = 1; i <=n; i++){
+  for(int j = 1; j <= i; j++){
+    cout<< i-j+1<<" ";
+  }
+  cout<<endl;
+ }
+
+ 
+// 1
+// 2 1
+// 3 2 1
+// 4 3 2 1
+// 5 4 3 2 1
+
+return 0;
+}
