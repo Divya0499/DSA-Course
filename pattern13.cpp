e54614b19ac29e5389012366a146f7c189b473b3
Divya <87847248+Divya0499@users.noreply.github.com>
2022-01-10 23:45:15 +0530

    Add files via upload

---
+++ pattern13.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int n;
+cin>>n;
+
+for(int i = 1; i <= n; i++){
+for(int space = n-i ; space >= 0; space--){
+  cout<<" ";
+}
+for(int j = 1; j <= i; j++){
+  cout<< "*";
+}
+cout<<endl;
+}
+
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+  return 0;
+}
