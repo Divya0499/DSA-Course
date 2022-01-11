44724eb1db5b4b799dd37ae19d49e92212c46c8e
Divya <87847248+Divya0499@users.noreply.github.com>
2022-01-11 22:47:02 +0530

    Update loop.cpp

--- loop.cpp
+++ loop.cpp
@@ -28,13 +28,75 @@ cout<<endl<<endl;
 int k;
 cin>>k;
 
-for(int i = 2; i < k; i++){
-  if( k % i == 0){
-    cout<<"not prime "<<i<<endl;
-  }else{
-    cout<<"prime "<<i<<endl;
+for(int i = 0; i <= 5; i++){
+  cout<< i << " ";
+  i++;  //output 0 2 4
+}
+
+for(int i = 0; i <= 5; i += 2){
+  cout<< i <<" ";
+  if(i&1){
+    continue;
+  }
+  i++; //output 0 3 5
+}
+
+for(int i = 0; i < 5; i++){
+  for(int j = i; j <= 5; j++){
+    cout<<i<<" "<<j<<endl;
+
+// output  0 0
+// 0 1
+// 0 2
+// 0 3
+// 0 4
+// 0 5
+// 1 1
+// 1 2
+// 1 3
+// 1 4
+// 1 5
+// 2 2
+// 2 3
+// 2 4
+// 2 5
+// 3 3
+// 3 4
+// 3 5
+// 4 4
+// 4 5
   }
 }
 
+for(int i = 0; i < 5; i++){
+  for(int j = i; j <= 5; j++){
+    if( i+j == 10){
+      break;
+    }
+    cout<< i<<" "<<j<<endl;
+
+
+//  output   0 0
+// 0 1
+// 0 2
+// 0 3
+// 0 4
+// 0 5
+// 1 1
+// 1 2
+// 1 3
+// 1 4
+// 1 5
+// 2 2
+// 2 3
+// 2 4
+// 2 5
+// 3 3
+// 3 4
+// 3 5
+// 4 4
+// 4 5
+  }
+}
   return 0;
 }
