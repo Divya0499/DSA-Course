1466b9829014da1f292eb9ab72a3959db72e0e02
Divya <87847248+Divya0499@users.noreply.github.com>
2022-01-11 22:11:44 +0530

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+int n;
+cin>>n;
+
+int a = 0;
+int b = 1;
+cout<<a<<" "<<b<<" ";
+for(int i = 1; i <= n; i++ ){
+int fib = a + b;
+cout<<fib<<" ";
+a = b;
+b = fib;
+
+
+}
+
+
+
+  return 0;
+}
