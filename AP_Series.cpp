6c8766aef87aa9bae5de7d7e74e4b4f166cdd488
Divya <87847248+Divya0499@users.noreply.github.com>
2022-01-12 18:55:03 +0530

    Add files via upload

---
+++ AP_Series.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int ApSeries(int n){
+
+int ans = 3*n+7;
+
+  return ans;
+}
+
+
+int main(){
+
+int n;
+cin>>n;
+ int ans = ApSeries(n);
+ cout<<ans;
+  return 0;
+}
